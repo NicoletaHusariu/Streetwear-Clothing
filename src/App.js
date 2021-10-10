784fbcb27d14b17c1e3117251bf85809bd002823
Nicoleta <nicoletaramonahusariu@gmail.com>
2021-10-10 14:42:15 +0300

    added progress bar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import HomePage from './pages/homepage/homepage.component';
 function App() {
 	return (
 		<div>
+			<div className="progress-bar-container">
+				<p>Work in progress</p>
+				<progress value="25" max="100">
+					70 %
+				</progress>
+			</div>
 			<HomePage />
 		</div>
 	);
